debaa365a3a93e1ba1a50dc3eaa65370fb2a28a6
Timur Sufiev <tsufiev@mirantis.com>
2013-08-14 12:02:11 +0400

    Add copyrights to .py and .js files.
    Change-Id: I13e6e15fd083ee42423fd22a5b1c3dadc9c7e7e8

--- muranodashboard/panel/services/__common.py
+++ muranodashboard/panel/services/__common.py
@@ -1,3 +1,17 @@
+#    Copyright (c) 2013 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import re
 import ast
 import json
@@ -487,9 +501,13 @@ class ServiceConfigurationForm(UpdatableFieldsForm):
                         return self.data.get(name, False)
 
                     def _set(field, value):
+                        # doesn't work - why?
+                        # super(field.__class__, field).__setattr__(key, value)
                         field.__dict__[key] = value
 
                     def _del(field):
+                        # doesn't work - why?
+                        # super(field.__class__, field).__delattr__(key)
                         del field.__dict__[key]
 
                     return key, property(_get, _set, _del)
