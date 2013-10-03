356474031d99362e38325015c019bea1aa23cf6e
efedorova <efedorova@mirantis.com>
2013-10-03 19:40:32 +0400

    Fix placeholder value saved on form submit in IE
    Change-Id: I8bb0ed5fda322e594604635d53b87ac91721507d

--- muranodashboard/static/muranodashboard/js/support_placeholder.js
+++ muranodashboard/static/muranodashboard/js/support_placeholder.js
@@ -33,5 +33,14 @@ $(function() {
                 input.val(input.attr('placeholder'));
               }
             }).blur();
-        }
+
+        $('[placeholder]').parents('form').submit(function() {
+          $(this).find('[placeholder]').each(function() {
+            var input = $(this);
+            if (input.val() == input.attr('placeholder')) {
+              input.val('');
+            }
+          })
+        });
+    }
 });
