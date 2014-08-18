859068a4e95b879547b73ff7228691805f5ff9f6
Ekaterina Fedorova <efedorova@mirantis.com>
2014-08-18 14:17:23 +0400

    Improve func test config file
    * Rename config_conf.conf to config.conf
    * Rename [common] - > [murano]
    * CommonGroup -> MuranoGroup
    * Also, hide outsider IP
    
    Change-Id: I2b9a0a85b5dbc4e53dd20249a85132222c6f1d7d

--- muranodashboard/tests/functional/config/config.py
+++ muranodashboard/tests/functional/config/config.py
@@ -1,9 +1,9 @@
 import os
 from oslo.config import cfg
 
-common_group = cfg.OptGroup(name='common', title="common configs")
+murano_group = cfg.OptGroup(name='murano', title="murano configs")
 
-CommonGroup = [
+MuranoGroup = [
     cfg.StrOpt('horizon_url',
                default='http://127.0.0.1/horizon',
                help="murano dashboard url"),
@@ -17,7 +17,7 @@ CommonGroup = [
                default='admin',
                help='keystone tenant'),
     cfg.StrOpt('keystone_url',
-               default='http://172.18.124.203:5000/v2.0/',
+               default='http://localhost:5000/v2.0/',
                help='keystone url'),
     cfg.StrOpt('murano_url',
                default='http://127.0.0.1:8082',
@@ -30,11 +30,11 @@ def register_config(config, config_group, config_opts):
     config.register_group(config_group)
     config.register_opts(config_opts, config_group)
 
-path = os.path.join(os.path.dirname(__file__), "config_file.conf")
+path = os.path.join(os.path.dirname(__file__), "config.conf")
 
 if os.path.exists(path):
     cfg.CONF([], project='muranodashboard', default_config_files=[path])
 
-register_config(cfg.CONF, common_group, CommonGroup)
+register_config(cfg.CONF, murano_group, MuranoGroup)
 
-common = cfg.CONF.common
+common = cfg.CONF.murano
