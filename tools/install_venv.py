b5b295c0aa8dea94c34bb7cbb2d5f3be6ffaee79
Ekaterina Chernova <efedorova@mirantis.com>
2015-03-11 17:45:01 +0300

    Update from openstack.common
    All missed modules were added to openstack-common.conf file
    and updated.
    
    Change-Id: Ia440c66820eec1d715813bece0a042d7f8855738

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -5,71 +5,67 @@
 # Copyright 2010 OpenStack Foundation
 # Copyright 2013 IBM Corp.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import os
-import ConfigParser
 import sys
 
-import install_venv_common as install_venv  # flake8: noqa
+import install_venv_common as install_venv  # noqa
 
 
-def print_help(project, venv, root):
+def print_help(venv, root):
     help = """
-  %(project)s development environment setup is complete.
+    OpenStack development environment setup is complete.
 
-  %(project)s development uses virtualenv to track and manage Python
-  dependencies while in development and testing.
+    OpenStack development uses virtualenv to track and manage Python
+    dependencies while in development and testing.
 
- To activate the %(project)s virtualenv for the extent of your current shell
- session you can run:
+    To activate the OpenStack virtualenv for the extent of your current shell
+    session you can run:
 
-    $ source %(venv)s/bin/activate
+    $ source %s/bin/activate
 
- Or, if you prefer, you can run commands in the virtualenv on a case by case
- basis by running:
+    Or, if you prefer, you can run commands in the virtualenv on a case by case
+    basis by running:
 
-    $ %(root)s/tools/with_venv.sh <your command>
+    $ %s/tools/with_venv.sh <your command>
 
+    Also, make test will automatically use the virtualenv.
     """
-    print help % dict(project=project, venv=venv, root=root)
+    print(help % (venv, root))
 
 
 def main(argv):
     root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
     if os.environ.get('tools_path'):
         root = os.environ['tools_path']
     venv = os.path.join(root, '.venv')
-    if os.environ.get('venv'):
-        venv = os.environ['venv']
+    if os.environ.get('VENV'):
+        venv = os.environ['VENV']
 
-    test_requires = os.path.join(root, 'test-requirements.txt')
     pip_requires = os.path.join(root, 'requirements.txt')
-
+    test_requires = os.path.join(root, 'test-requirements.txt')
     py_version = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
-    setup_cfg = ConfigParser.ConfigParser()
-    setup_cfg.read('setup.cfg')
-    project = setup_cfg.get('metadata', 'name')
-
-    install = install_venv.InstallVenv(root, venv, pip_requires,
-                                       test_requires, py_version, project)
+    project = 'OpenStack'
+    install = install_venv.InstallVenv(root, venv, pip_requires, test_requires,
+                                       py_version, project)
     options = install.parse_args(argv)
     install.check_python_version()
     install.check_dependencies()
     install.create_virtualenv(no_site_packages=options.no_site_packages)
     install.install_dependencies()
-    install.post_process()
-    print_help(project, venv, root)
+    print_help(venv, root)
 
 if __name__ == '__main__':
     main(sys.argv)
