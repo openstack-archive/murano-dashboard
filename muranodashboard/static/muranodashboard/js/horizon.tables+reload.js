6aba78cac65ecdfeda5ef93dd8c0010a4a8c8a03
Kirill Zaitsev <kzaitsev@mirantis.com>
2015-05-22 18:58:18 +0300

    Call location.reload only once at successfull env deployment
    Before it was possible, that in some configurations table update events
    would stack and reload would be called multiple times.
    This change introduces a global js variable, that would prevent
    location.reload from being called more than once.
    
    
    Change-Id: Idb332e56b764d494a51de74398e7098c68561c61
    Related-Bug: #1454221

--- muranodashboard/static/muranodashboard/js/horizon.tables+reload.js
+++ muranodashboard/static/muranodashboard/js/horizon.tables+reload.js
@@ -1,9 +1,16 @@
+// In some cases successfull update events can stack up in case we have lots of apps in an env.
+// This might lead to a situation when lots of reloads are scheduled simultaneously.
+// The following variable forces reload to be called only once.
+var reload_called = false;
 $(function() {
   $("table#services.datatable").on("update", function () {
     // If every component has finished installing (with error or success): reloads the page.
     var rows_to_update = $(this).find('tr.status_unknown.ajax-update');
     if (rows_to_update.length === 0) {
-      location.reload(true);
+      if (reload_called === false) {
+        reload_called = true;
+        location.reload(true);
+      }
     }
   });
 });
