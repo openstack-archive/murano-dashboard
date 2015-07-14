cdb94feb2ffffab8f08355de4f2238348ee84a81
Alexander Tivelkov <ativelkov@mirantis.com>
2015-07-14 14:41:41 +0300

    Improve the checks of Dynamic UI format versions
    Current implementation of DynamicUI checks the "Version" field of UI
    definition files to exactly match a single constant value (currently 2).
    This contradicts to our desired versioning strategy.
    
    Instead, it should check that the major component of the Version field
    in the UI definition being processed  matches tha major component of the
    current latest version of the format (i.e. we assume that the versions
    are compatible if their major version are the same). At the same time
    the version of the definition being processed is not greater than the
    latest supported version.
    
    The implementation is done using the semantic_version library which has
    all the needed functionality, including the coercing method (parsing of
    string-based content into the proper version) and partial versions.
    
    Targets Blueprint murano-versioning
    
    Change-Id: I96cbbddc4389804fafe9a1d6f48f63ac4ee5620a

--- muranodashboard/dynamic_ui/version.py
+++ muranodashboard/dynamic_ui/version.py
@@ -12,10 +12,22 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-VERSION = 2
+import semantic_version
+
+LATEST_FORMAT_VERSION = 2.0
 
 
 def check_version(version):
-    msg = 'Unsupported Dynamic UI version: required {0}, got {1}'
-    if version != VERSION:
-        raise ValueError(msg.format(VERSION, version))
+    latest = semantic_version.Version.coerce(str(LATEST_FORMAT_VERSION))
+    supported = semantic_version.Version(str(latest.major), partial=True)
+    requested = semantic_version.Version.coerce(str(version))
+    if supported != requested:
+        msg = 'Unsupported Dynamic UI format version: ' \
+              'requested format version {0} is not compatible with the ' \
+              'supported family {1}'
+        raise ValueError(msg.format(requested, supported))
+    if requested > latest:
+        msg = 'Unsupported Dynamic UI format version: ' \
+              'requested format version {0} is newer than ' \
+              'latest supported {1}'
+        raise ValueError(msg.format(requested, latest))
