a21d86b0680150195cdfa370ee658b8496ab9b7d
Kirill Zaitsev <kzaitsev@mirantis.com>
2015-07-24 02:31:34 +0300

    eslint cleanup of smaller js files
    Added indentation check to .eslintrc
    Cleaned up horizon.tables+reload.js, mixed-mode.js, submit-disabled.js,
    upload_form.js, according to eslint rules.
    Added missing license info to horizon.tables+reload.js
    
    Partially implements: blueprint add-js-lint-jobs
    
    Change-Id: If417405c24eaa0c65b8093e559730e83bd070b57

--- muranodashboard/static/muranodashboard/js/mixed-mode.js
+++ muranodashboard/static/muranodashboard/js/mixed-mode.js
@@ -13,15 +13,16 @@
     under the License.
 */
 $(function() {
-    function check_mixed_mode(){
-        var checked = $("input[id*='mixedModeAuth']").prop('checked')
-        if ( checked === true) {
-            $("label[for*='saPassword']").parent().css({'display': 'inline-block'});
-        } else if (checked === false) {
-            $("label[for*='saPassword']").parent().css({'display': 'none'});
-        }
+  "use strict";
+  function checkMixedMode(){
+    var checked = $("input[id*='mixedModeAuth']").prop('checked');
+    if ( checked === true) {
+      $("label[for*='saPassword']").parent().css({'display': 'inline-block'});
+    } else if (checked === false) {
+      $("label[for*='saPassword']").parent().css({'display': 'none'});
     }
+  }
 
-    $("input[id*='mixedModeAuth']").change(check_mixed_mode);
-    check_mixed_mode();
+  $("input[id*='mixedModeAuth']").change(checkMixedMode);
+  checkMixedMode();
 });
