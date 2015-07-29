ec22c93fbe7dfad52000ad50e561951a59ddc18d
xiangxinyong <xiangxinyong@huawei.com>
2015-07-29 11:27:39 +0800

    eslint cleanup of external-ad.js
    Cleaned up external-ad.js to make it pass eslint
    
    Partially implements: blueprint add-js-lint-jobs
    
    Change-Id: I1053edadebfe505a8b5f45d11381686745b640c1

--- muranodashboard/static/muranodashboard/js/external-ad.js
+++ muranodashboard/static/muranodashboard/js/external-ad.js
@@ -13,20 +13,21 @@
     under the License.
 */
 $(function() {
-    function check_preconfigured_ad(){
-        var checked = $("input[id*='externalAD']").prop('checked')
-        if ( checked== true) {
-            $("select[id*='-domain']").attr("disabled", "disabled");
-            $("label[for*='domainAdminUserName']").parent().css({'display': 'inline-block'});
-            $("label[for*='domainAdminPassword']").parent().css({'display': 'inline-block'});
-        }
-        if (checked == false) {
-            $("select[id*='-domain']").removeAttr("disabled");
-            $("label[for*='domainAdminUserName']").parent().css({'display': 'none'});
-            $("label[for*='domainAdminPassword']").parent().css({'display': 'none'});
-        }
+  "use strict";
+  function checkPreconfiguredAd(){
+    var checked = $("input[id*='externalAD']").prop('checked');
+    if (checked === true) {
+      $("select[id*='-domain']").attr("disabled", "disabled");
+      $("label[for*='domainAdminUserName']").parent().css({'display': 'inline-block'});
+      $("label[for*='domainAdminPassword']").parent().css({'display': 'inline-block'});
     }
+    if (checked === false) {
+      $("select[id*='-domain']").removeAttr("disabled");
+      $("label[for*='domainAdminUserName']").parent().css({'display': 'none'});
+      $("label[for*='domainAdminPassword']").parent().css({'display': 'none'});
+    }
+  }
 
-    $("input[id*='externalAD']").change(check_preconfigured_ad);
-    check_preconfigured_ad();
+  $("input[id*='externalAD']").change(checkPreconfiguredAd);
+  checkPreconfiguredAd();
 });
