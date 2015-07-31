59d4027d13792468fb8b33385f87643f8d14ba3f
Ekaterina Chernova <efedorova@mirantis.com>
2015-07-31 11:45:55 +0300

    Add category list pagination support
    Pagination was added to the 'Manage->Categories' page.
    
    For getting category list 'catalog/categories' is used
    everywhere. Before there were places where 'catalog/packages/categories' used.
    This call is deprecated.
    
    Also added handle of BadRequest error to common exception handler,
    so if invalid sort key is passed, dashboard will stay alive.
    
    Change-Id: I80758dce32fc3930250f8924d3243a7a1cdbda80
    Related-bug: #1474932

--- muranodashboard/categories/views.py
+++ muranodashboard/categories/views.py
@@ -12,11 +12,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import itertools
+
 from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
-
 from horizon.forms import views
 from horizon import tables as horizon_tables
+from horizon.utils import functions as utils
 
 from muranodashboard import api
 from muranodashboard.categories import forms
@@ -27,11 +29,56 @@ class CategoriesView(horizon_tables.DataTableView):
     table_class = tables.CategoriesTable
     template_name = 'categories/index.html'
 
+    def has_prev_data(self, table):
+        return self._prev
+
+    def has_more_data(self, table):
+        return self._more
+
     def get_data(self):
+        prev_marker = self.request.GET.get(
+            tables.CategoriesTable._meta.prev_pagination_param, None)
+
+        if prev_marker is not None:
+            sort_dir = 'asc'
+            marker = prev_marker
+        else:
+            sort_dir = 'desc'
+            marker = self.request.GET.get(
+                tables.CategoriesTable._meta.pagination_param, None)
+
+        page_size = utils.get_page_size(self.request)
+
+        request_size = page_size + 1
+        kwargs = {'filters': {}}
+        if marker:
+            kwargs['marker'] = marker
+        kwargs['sort_dir'] = sort_dir
+
         categories = []
+        self._prev = False
+        self._more = False
         with api.handled_exceptions(self.request):
-            categories = api.muranoclient(self.request).categories.list()
-        return sorted(categories, key=lambda category: category.name.lower())
+            categories_iter = api.muranoclient(self.request).categories.list(
+                limit=request_size, **kwargs)
+
+            categories = list(itertools.islice(categories_iter, request_size))
+            # first and middle page condition
+            if len(categories) > page_size:
+                categories.pop(-1)
+                self._more = True
+                # middle page condition
+                if marker is not None:
+                    self._prev = True
+            # first page condition when reached via prev back
+            elif sort_dir == 'asc' and marker is not None:
+                self._more = True
+            # last page condition
+            elif marker is not None:
+                self._prev = True
+            if prev_marker is not None:
+                categories.reverse()
+        return categories
 
 
 class AddCategoryView(views.ModalFormView):
