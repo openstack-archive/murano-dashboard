3bc5d09e3b706135c153c42fbb792ff688668df4
zhu.rong <zhu.rong@99cloud.net>
2015-09-15 22:29:42 +0800

    Only allow http/https schemas for Import Bundle/Package
    add validator restricts schema to http/https for Import Bundle/Package
    
    This patch is inspired from https://review.openstack.org/#/c/220359/
    
    Change-Id: I6b82bc1cbf90cfedaa0b170cc3a00b5f51cba3b2
    Closes-Bug: #1495997

--- muranodashboard/packages/forms.py
+++ muranodashboard/packages/forms.py
@@ -16,6 +16,7 @@ import json
 import sys
 
 from django.core.urlresolvers import reverse
+from django.core import validators
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from horizon import exceptions
@@ -56,6 +57,8 @@ class ImportBundleForm(forms.Form):
             'class': 'switched',
             'data-switch-on': 'source',
             'data-source-by_url': _('Bundle URL')}),
+        validators=[validators.URLValidator(
+            schemes=["http", "https"])],
         help_text=_('An external http/https URL to load the bundle from.'))
     name = forms.CharField(
         label=_("Bundle Name"),
@@ -92,6 +95,8 @@ class ImportPackageForm(forms.Form):
             'class': 'switched',
             'data-switch-on': 'source',
             'data-source-by_url': _('Package URL')}),
+        validators=[validators.URLValidator(
+            schemes=["http", "https"])],
         help_text=_('An external http/https URL to load the package from.'))
     repo_name = horizon_forms.CharField(
         label=_("Package Name"),
