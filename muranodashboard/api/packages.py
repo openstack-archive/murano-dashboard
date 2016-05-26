66ef3d71941c4bd672d1e8e37d8f7b199acd5462
Kirill Zaitsev <kzaitsev@mirantis.com>
2016-05-27 00:11:28 +0300

    Inherit custom yaml Loader from yaml.SafeLoader
    Before this patch yaql-enabled yaml Loader was inherited from yaml.Loader, that
    potentially allows creating arbitrary python objects from specifically
    formatted yaml tags. This could have happened whenever UI definitions of
    the package were processed.
    With this change yaql yaml-Loader no longer allows creating custom python objects.
    
    Change-Id: I4fe38aa7e0fc567211ab872c7e1f8e81dbc3e765
    Closes-Bug: #1586079

--- muranodashboard/api/packages.py
+++ muranodashboard/api/packages.py
@@ -71,7 +71,7 @@ def app_by_fqn(request, fqn, catalog=True):
 
 
 def make_loader_cls():
-    class Loader(yaml.Loader):
+    class Loader(yaml.SafeLoader):
         pass
 
     def yaql_constructor(loader, node):
@@ -80,7 +80,7 @@ def make_loader_cls():
 
     # workaround for PyYAML bug: http://pyyaml.org/ticket/221
     resolvers = {}
-    for k, v in yaml.Loader.yaml_implicit_resolvers.items():
+    for k, v in yaml.SafeLoader.yaml_implicit_resolvers.items():
         resolvers[k] = v[:]
     Loader.yaml_implicit_resolvers = resolvers
 
