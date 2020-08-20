5915355c5b55101ca578c5b0d8cf7828a15460a5
manchandavishal <manchandavishal143@gmail.com>
2020-08-20 14:20:07 +0000

    [goal] Migrate testing to ubuntu focal
    If we move horizon jobs to focal then murano-dashboard nodejs jobs
    fail. This patch the updates python version to py38 in karma.conf.js
    and tools/post_install.sh files as py36 is not avaialbale on focal
    which fixes failed nodejs job.
    
    Depends-On: https://review.opendev.org/#/c/744647/
    
    Change-Id: Ic72729514d4759466e8afeac6d03cb1ed7a1765d

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ var path = require('path');
 
 module.exports = function (config) {
   // This tox venv is setup in the post-install npm step
-  var toxPath = '.tox/py36/lib/python3.6/site-packages/';
+  var toxPath = '.tox/py38/lib/python3.8/site-packages/';
   var xstaticPath = toxPath + 'xstatic/pkg/';
 
   config.set({
